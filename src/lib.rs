b29a335e25f1fd1016c87f58c81b0ff59f7cc765
Paul Sochiera <paul@sochiera.com>
2024-01-08 14:20:32 +0100

    feat: basic CRUD with diesel

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use diesel::mysql::MysqlConnection;
+pub mod models;
+pub mod schema;
+
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
 
-pub mod models;
-pub mod schema;
+use self::models::{NewPost, Post};
 
 pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
@@ -12,4 +13,23 @@ pub fn establish_connection() -> MysqlConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     MysqlConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+
+pub fn create_post(conn: &mut MysqlConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    conn.transaction(|conn| {
+        diesel::insert_into(posts::table)
+            .values(&new_post)
+            .execute(conn)?;
+
+        posts::table
+            .order(posts::id.desc())
+            .select(Post::as_select())
+            .first(conn)
+    })
+    .expect("Error while saving post")
 }
