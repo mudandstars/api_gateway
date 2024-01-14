6c80b346c7386d6770f0ecf97eb62d122da05dec
Paul Sochiera <paul@sochiera.com>
2024-01-14 14:30:47 +0100

    feat: simple rocket setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/world")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/world2")]
+fn index2() -> &'static str {
+    "Hello, world2!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/hello", routes![index, index2])
 }
