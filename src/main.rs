8754f7ab99a4029631f37249667bc285387e2589
Paul Sochiera <paul@sochiera.com>
2024-02-10 12:09:49 -0500

    feat: removed unused tracing libs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use dotenvy::dotenv;
 use std::net::SocketAddr;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use api_gateway::{
     app::{create_app, mysql_pool},
@@ -9,18 +8,9 @@ use api_gateway::{
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-
     dotenv().ok();
 
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
     let pool = mysql_pool(&db_url);
 
     let logger_layer = RequestLoggerLayer::new(pool.clone());
@@ -31,7 +21,6 @@ async fn main() {
         .layer(logger_layer);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {addr}");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
